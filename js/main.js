2a88e9a76570216d7e55a92ed375de6f8326ae40
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-02-21 23:52:33 +0100

    condizioni x far apparire i dati utente su pag web

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@
 
 /*- impostare variabile con valore prompt con la quale chiedere
 all'utente il numero di km che deve percorrere (variabile const)*/
-const kmTravel = parseInt (promp ('PREGO INSERIRE IL NUMERO DI KM DA PERCORRERE'));
+const kmTravel = parseInt (prompt ('PREGO INSERIRE IL NUMERO DI KM DA PERCORRERE'));
 
 /*- impostare variabile con valore prompt con la quale chiedere
 all'utente l'età (variabile const)*/
-const etaUser = parseInt (promp('PREGO INSERIRE IL NUMERO DI ANNI'));
+const etaUser = parseInt (prompt ('PREGO INSERIRE IL NUMERO DI ANNI'));
 
 /*- impostare una variabile prezzo base del biglietto che è di 
 0,21 € per km (variabile let)*/
@@ -14,11 +14,11 @@ let basePrice = parseFloat (0.21);
 
 /*- impostare una variabile prezzo minorenni che parta dal prezzo 
 base e lo sconti del 20% (variabile let)*/
-let under18Price = parseFloat (basePrice /100 *80);
+let under18Price = parseFloat (basePrice /100 *80).toFixed(2);
 
 /*- impostare una variabile prezzo over65 che parta dal prezzo 
 base e lo sconti del 40% (variabile let)*/
-let over65Price = parseFloat (basePrice /100 *60);
+let over65Price = parseFloat (basePrice /100 *60).toFixed(2);
 
 
 /*-Sulla base delle precedenti informazioni fornite dall'utente:
@@ -34,5 +34,37 @@ if (etaUser < 18){
 
 }
 
-// provare con funzione switch se ha senso usarla, e provare
-// ad inserire alcuni dati html e css-
+
+/*Impostazione per mostrare sulla pagina web la tariffa di partenza a seconda dei 
+dati compilati dall'utente*/
+if (etaUser < 18){
+    document.getElementById('nb_age_user').innerHTML =  `La tua tariffa: ${under18Price} € - (sconto 20% minorenni)`;
+    
+    
+} else if (etaUser > 65){
+    document.getElementById('nb_age_user').innerHTML =  `La tua tariffa: ${over65Price} € - sconto 40% over65`;
+
+} else {
+    document.getElementById('nb_age_user').innerHTML =  `La tua tariffa: ${basePrice} € - tariffa base`;
+
+
+}
+
+/*Impostazione per mostrare sulla pagina web i km da percorrere a seconda dei 
+dati compilati dall'utente*/
+document.getElementById('nb_km_user').innerHTML =  `Chilometri da percorrere: ${kmTravel} km`;
+
+
+/*Impostazione per mostrare sulla pagina web il totale da pagare a seconda dei 
+dati compilati dall'utente*/
+if (etaUser < 18){
+    document.getElementById('nb_final_price').innerHTML ='Prezzo totale da pagare: ' + (kmTravel * under18Price).toFixed(2) + "€";
+    
+} else if (etaUser > 65){
+    document.getElementById('nb_final_price').innerHTML ='Prezzo totale da pagare: ' + (kmTravel * over65Price).toFixed(2) + "€";
+
+} else {
+    document.getElementById('nb_final_price').innerHTML ='Prezzo totale da pagare: ' + (kmTravel * basePrice).toFixed(2) + "€";
+
+}
+
